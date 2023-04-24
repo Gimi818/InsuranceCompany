92ca61883205768a85c7aeb363ed47385039ab74
Gimi818 <gimi8183@gmail.com>
2023-04-24 13:34:57 +0200

    Initial commit

---
+++ src/test/java/com/carinsurance/CarInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInsuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
