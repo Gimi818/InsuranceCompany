d8ccc952f0dc8fc82405bb067cb865880218e321
Gimi818 <gimi8183@gmail.com>
2023-05-14 15:30:16 +0200

    added new tests

--- src/test/java/com/carinsurance/policy/PolicyControllerTest.java
+++ src/test/java/com/carinsurance/policy/PolicyControllerTest.java
@@ -1,6 +1,9 @@
 package com.carinsurance.policy;
 
 
+import com.carinsurance.car.CarModel;
+import com.carinsurance.car.dto.CarResponseDto;
+import com.carinsurance.client.dto.ClientResponseDto;
 import com.carinsurance.policy.dto.PolicyRequestDto;
 import com.carinsurance.policy.dto.PolicyResponseDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,6 +18,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,6 +28,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,6 +48,8 @@ public class PolicyControllerTest {
     private static String clientRequestDtoJson;
     private static String policyRequestDtoJson;
     private static PolicyRequestDto policyRequestDto;
+    private static CarResponseDto carResponseDto;
+    private static ClientResponseDto clientResponseDto;
 
 
     @BeforeEach
@@ -53,12 +60,12 @@ public class PolicyControllerTest {
 
         policyResponseDto = new PolicyResponseDto(1L, 1000, LocalDate.now(), LocalDate.now().plusYears(1));
         policyRequestDto = new PolicyRequestDto(1000, LocalDate.now(), LocalDate.now().plusYears(1));
-        // clientRequestDtoJson = objectMapper.writeValueAsString(policyRequestDto);
+
     }
 
     @Test
     void should_find_policy_by_id() throws Exception {
-        BDDMockito.given(policyService.findPolicyById(1L)).willReturn(policyResponseDto);
+        given(policyService.findPolicyById(1L)).willReturn(policyResponseDto);
         mockMvc.perform(get("/policies/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -68,18 +75,19 @@ public class PolicyControllerTest {
                 .andExpect(jsonPath("$.endDate").value(LocalDate.now().plusYears(1).format(DateTimeFormatter.ISO_LOCAL_DATE)));
     }
 
-//    @Test
-//    public void shouldSavePolicy() throws Exception {
-//        // given
-//        Long clientId = 1L;
-//        Long carId = 1L;
-//
-//        given(policyService.savePolicy(clientId, carId)).willReturn(policy);
-//
-//        mockMvc.perform((RequestBuilder) post("/clients/{clientId}/cars/{carId}", clientId, carId)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isCreated());
-//
-//
-//    }
+    @Test
+    public void shouldSavePolicy() throws Exception {
+        // given
+        carResponseDto = new CarResponseDto(1L, "Bmw", "X5", 30000, CarModel.CAR, 2010, 3.0, 21000, null);
+        clientResponseDto = new ClientResponseDto(1L, "John", "Smith", 30, null);
+
+
+        given(policyService.savePolicy(clientResponseDto.id(), carResponseDto.id())).willReturn(policy);
+
+        mockMvc.perform(post("/policies/1/cars/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+
+    }
 }
