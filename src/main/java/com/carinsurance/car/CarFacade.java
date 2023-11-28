3d709713b16d15c47b3b93d7b0c4a365f1f6e22e
Gimi818 <gimi8183@gmail.com>
2023-11-28 16:53:40 +0100

    Added method findCarWithoutPolicyForClient

--- src/main/java/com/carinsurance/car/CarFacade.java
+++ src/main/java/com/carinsurance/car/CarFacade.java
@@ -5,8 +5,11 @@ import com.carinsurance.car.dto.CarResponseDto;
 
 public interface CarFacade {
 
+    Car saveCarWithAddedInsurance(Car car);
 
     Car saveCar(CarRequestDto carRequestDto, Long clientId);
+
+    Car findCarWithoutPolicyForClient(Long clientId);
 
     Car findCarById(Long id);
 }
--- src/main/java/com/carinsurance/car/CarService.java
+++ src/main/java/com/carinsurance/car/CarService.java
@@ -2,60 +2,60 @@ package com.carinsurance.car;
 
 import com.carinsurance.car.dto.CarRequestDto;
 import com.carinsurance.car.dto.CarResponseDto;
-import com.carinsurance.car.exception.CarHasAPolicyException;
-import com.carinsurance.car.exception.CarNotFoundException;
-import com.carinsurance.policy.Policy;
-import com.carinsurance.policy.PolicyRepository;
-import com.carinsurance.policy.exception.PolicyNotFoundException;
 
+import com.carinsurance.client.Client;
+import com.carinsurance.client.ClientFacade;
+
+import com.carinsurance.common.exception.exceptions.NotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 @AllArgsConstructor
 @Log4j2
-public class CarService {
-
+public class CarService implements CarFacade {
+    private final ClientFacade clientFacade;
     private final CarRepository carRepository;
     private final CarMapper carMapper;
-    private final PolicyRepository policyRepository;
 
 
-
-    public Car saveCar(CarRequestDto carRequestDto) {
-        log.info("Saving car {}", carRequestDto);
+    public Car saveCar(CarRequestDto carRequestDto, Long clientId) {
         Car newCar = carRepository.save(carMapper.dtoToEntity(carRequestDto));
         log.info("Saved car {}", newCar);
+        Client client = clientFacade.findById(clientId);
+        client.getCars().add(newCar);
+        clientFacade.saveClientWithAddedCar(client);
         return newCar;
     }
 
+    public Car saveCarWithAddedInsurance(Car car) {
+        carRepository.save(car);
+        return car;
+    }
 
-    public CarResponseDto findCarById(Long id) {
-        log.info("Finding car with ID {}", id);
+    public Car findCarWithoutPolicyForClient(Long clientId) {
+        Client client = clientFacade.findById(clientId);
+        List<Car> cars = client.getCars();
+        for (Car car : cars) {
+            if (car.getPolicy() == null) {
+                return car;
+            }
+            throw new NotFoundException("Car has policy");
+        }
+        throw new NotFoundException(clientId);
+    }
 
-        Car car = carRepository.findById(id).orElseThrow(() -> new CarNotFoundException(id));
+    public Car findCarById(Long id) {
+        Car car = carRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
         log.info("Found car {}", car);
 
-        return carMapper.entityToDto(car);
+        return car;
     }
 
-
-    public Car assignPolicyToCar(Long carId, Long policyId) {
-        log.info("Assigning policy with ID {} to car with ID {}", policyId, carId);
-
-        Car car = carRepository.findById(carId).orElseThrow(() -> new CarNotFoundException(carId));
-        Policy policy = policyRepository.findById(policyId).orElseThrow(() -> new PolicyNotFoundException(policyId));
-
-        if (car.getPolicy() != null) {
-            throw  new CarHasAPolicyException(carId);
-        }
-        car.setPolicy(policy);
-
-        log.info("Assigned policy with ID {} to car with ID {}", policyId, carId);
-        return carRepository.save(car);
-    }
 
 }
 
