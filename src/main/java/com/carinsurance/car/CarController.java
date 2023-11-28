23437b74d5641d5b23b8ac23ec00c4cb7feac8ca
Gimi818 <gimi8183@gmail.com>
2023-11-28 14:05:08 +0100

    Added Routes and deleted method

--- src/main/java/com/carinsurance/car/CarController.java
+++ src/main/java/com/carinsurance/car/CarController.java
@@ -1,38 +1,32 @@
 package com.carinsurance.car;
 
 import com.carinsurance.car.dto.CarRequestDto;
-import com.carinsurance.car.dto.CarResponseDto;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.carinsurance.car.CarController.Routes.*;
 
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/cars")
+
 public class CarController {
 
     private final CarService carService;
 
-    @PostMapping("/add")
-    public ResponseEntity<Car> saveCar(@RequestBody CarRequestDto carRequestDto) {
-        return new ResponseEntity<>(carService.saveCar(carRequestDto), HttpStatus.CREATED);
+    @PostMapping(ROOT)
+    public ResponseEntity<Car> saveCar(@RequestBody CarRequestDto car, @PathVariable Long clientId) {
+        return new ResponseEntity<>(carService.saveCar(car, clientId), HttpStatus.CREATED);
     }
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CarResponseDto> findCarById(@PathVariable Long id) {
-        CarResponseDto carResponseDto = carService.findCarById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(carResponseDto);
+    static final class Routes {
+        static final String ROOT = "/cars";
 
-    }
 
-    @PutMapping("/{carId}/policies/{policyId}")
-    public ResponseEntity<Void> addPolicyToCar(@PathVariable Long carId, @PathVariable Long policyId) {
-        carService.assignPolicyToCar(carId, policyId);
-        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/com/carinsurance/policy/PolicyController.java
+++ src/main/java/com/carinsurance/policy/PolicyController.java
@@ -1,34 +1,35 @@
 package com.carinsurance.policy;
 
 
-import com.carinsurance.policy.dto.PolicyResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.carinsurance.policy.PolicyController.Routes.*;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/policies")
 public class PolicyController {
     private final PolicyService policyService;
 
-    @PostMapping("/OC/{clientId}/cars/{carId}")
+    @PostMapping(OC)
     public ResponseEntity<Policy> saveAcInsurance(@PathVariable Long clientId, @PathVariable Long carId) {
 
         return new ResponseEntity<>(policyService.saveOCPolicy(clientId, carId), HttpStatus.CREATED);
     }
 
-    @PostMapping("/OC/AC/{clientId}/cars/{carId}")
+    @PostMapping(OCAC)
     public ResponseEntity<Policy> saveAcAndOcInsurance(@PathVariable Long clientId, @PathVariable Long carId) {
 
         return new ResponseEntity<>(policyService.saveACAndOCPolicy(clientId, carId), HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PolicyResponseDto> findPolicyById(@PathVariable Long id) {
-        PolicyResponseDto policyResponseDto = policyService.findPolicyById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(policyResponseDto);
-    }
+    static final class Routes {
+        static final String ROOT = "/policies";
+        static final String OC = ROOT + "/OC/{clientId}/{carId}";
+        static final String OCAC = ROOT + "/OC/AC/{clientId}/{carId}";
 
+
+    }
 }
